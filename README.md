4db618b9def1b5c92dc8eb456071be5325954baf
DJAMohamed <poupartmanu@yahoo.fr>
2021-10-01 14:20:02 +0200

    fix: modification d'emplacement de la BDD docker et correction dans DeplacementRepository

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-"docker compose up" dans le dossier "db" pour monter la database mysql.
+"docker compose up" pour monter la database mysql.
+
+
+Exemple de Json à POST sur /infirmieres :
+
+    {
+    "numeroProfessionnel":"545155458",
+    "nom":"Dupuis",
+    "prenom":"Bidule",
+    "telPro":"06452178987",
+    "telPerso":"078721581228",
+    "adresse":{"id":1 }
+    }
+
+
+Exemple de Json à POST sur /patients :
+
+    {
+    "nom":"Valjean",
+    "prenom":"Jean",
+    "dateDeNaissance":"1970-10-02",
+    "sexe":"Masculin",
+    "numeroSecuriteSociale":"41541315",
+    "adresse":{"id":1},
+    "infirmiere":{"id":1}
+    }
--- src/main/java/fr/semifir/cabinet_infirmier/repositories/DeplacementRepository.java
+++ src/main/java/fr/semifir/cabinet_infirmier/repositories/DeplacementRepository.java
@@ -3,14 +3,16 @@ package fr.semifir.cabinet_infirmier.repositories;
 import fr.semifir.cabinet_infirmier.models.Deplacement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface DeplacementRepository extends JpaRepository<Deplacement, Integer> {
 
-    @Query("SELECT * FROM deplacement WHERE infirmiere_id = ?1")
+    @Query("SELECT d FROM Deplacement d WHERE d.infirmiere=?1")
     List<Deplacement> findByInfirmiereId(Integer id);
 
-    @Query("SELECT * FROM deplacement WHERE patient_id = ?1")
+    @Query("SELECT d FROM Deplacement d WHERE d.patient=?1")
     List<Deplacement> findByPatientId(Integer id);
 }
